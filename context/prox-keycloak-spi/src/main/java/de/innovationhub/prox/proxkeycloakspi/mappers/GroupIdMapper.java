dc42e2e8746c9cce23a11a3fec6f5ff3c8a2540f
tobi6112 <22715034+tobi6112@users.noreply.github.com>
2021-09-07 11:42:22 +0200

    Add Group ID Mapper to Keycloak deployment

---
+++ context/prox-keycloak-spi/src/main/java/de/innovationhub/prox/proxkeycloakspi/mappers/GroupIdMapper.java
@@ -0,0 +1,83 @@
+package de.innovationhub.prox.proxkeycloakspi.mappers;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.jboss.logging.Logger;
+import org.keycloak.models.ClientSessionContext;
+import org.keycloak.models.KeycloakSession;
+import org.keycloak.models.ProtocolMapperModel;
+import org.keycloak.models.UserSessionModel;
+import org.keycloak.protocol.oidc.mappers.AbstractOIDCProtocolMapper;
+import org.keycloak.protocol.oidc.mappers.OIDCAccessTokenMapper;
+import org.keycloak.protocol.oidc.mappers.OIDCAttributeMapperHelper;
+import org.keycloak.protocol.oidc.mappers.OIDCIDTokenMapper;
+import org.keycloak.protocol.oidc.mappers.UserInfoTokenMapper;
+import org.keycloak.provider.ProviderConfigProperty;
+import org.keycloak.representations.IDToken;
+
+public class GroupIdMapper extends AbstractOIDCProtocolMapper implements OIDCAccessTokenMapper, OIDCIDTokenMapper, UserInfoTokenMapper {
+
+    /*
+     * A config which keycloak uses to display a generic dialog to configure the token.
+     */
+    private static final List<ProviderConfigProperty> configProperties = new ArrayList<>();
+
+    private static final Logger log = Logger.getLogger(GroupIdMapper.class);
+
+    /*
+     * The ID of the token mapper. Is public, because we need this id in our data-setup project to
+     * configure the protocol mapper in keycloak.
+     */
+    public static final String PROVIDER_ID = "oidc-group-id-mapper";
+
+    static {
+        // The builtin protocol mapper let the user define under which claim name (key)
+        // the protocol mapper writes its value. To display this option in the generic dialog
+        // in keycloak, execute the following method.
+        OIDCAttributeMapperHelper.addTokenClaimNameConfig(configProperties);
+        // The builtin protocol mapper let the user define for which tokens the protocol mapper
+        // is executed (access token, id token, user info). To add the config options for the different types
+        // to the dialog execute the following method. Note that the following method uses the interfaces
+        // this token mapper implements to decide which options to add to the config. So if this token
+        // mapper should never be available for some sort of options, e.g. like the id token, just don't
+        // implement the corresponding interface.
+        OIDCAttributeMapperHelper.addIncludeInTokensConfig(configProperties, GroupIdMapper.class);
+        
+    }
+
+    @Override
+    public String getDisplayCategory() {
+        return "Token mapper";
+    }
+
+    @Override
+    public String getDisplayType() {
+        return "Group ID Mapper";
+    }
+
+    @Override
+    public String getId() {
+        return PROVIDER_ID;
+    }
+
+    @Override
+    public List<ProviderConfigProperty> getConfigProperties() {
+        return configProperties;
+    }
+
+    @Override
+    public String getHelpText() {
+        return "Maps the Group IDs to the claim";
+    }
+
+    @Override
+    protected void setClaim(IDToken token, ProtocolMapperModel mappingModel, UserSessionModel userSession, KeycloakSession keycloakSession,
+                            ClientSessionContext clientSessionCtx) {
+        var group_ids = userSession.getUser().getGroups().stream().map(group -> group.getId()).collect(Collectors.toSet());
+        String protocolClaim = mappingModel.getConfig().get(OIDCAttributeMapperHelper.TOKEN_CLAIM_NAME);
+        token.getOtherClaims().put(protocolClaim, group_ids);
+    }
+    
+}
